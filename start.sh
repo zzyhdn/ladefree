c843f7f7eb9c870a02342cc9a1c762ba340da2dc
zzyhdn <32586995+zzyhdn@users.noreply.github.com>
2025-05-29 12:30:49 +0800

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash  
-export UUID=${UUID:-'5ecc0799-1464-48b1-87c0-bbdeb63823fc'} # 哪吒v1,在不同的平台部署需要改UUID，否则会覆盖
+export UUID=${UUID:-'b4e03573-fb05-41db-ba29-16a8c69ed87b'} # 哪吒v1,在不同的平台部署需要改UUID，否则会覆盖
 export NEZHA_SERVER=${NEZHA_SERVER:-''}       # v1哪吒填写形式：nezha.abc.com:8008,v0哪吒填写形式：nezha.abc.com
 export NEZHA_PORT=${NEZHA_PORT:-''}           # v1哪吒不要填写这个,v0哪吒agent端口为{443,8443,2053,2083,2087,2096}其中之一时自动开启tls
 export NEZHA_KEY=${NEZHA_KEY:-''}             # v1的NZ_CLIENT_SECRET或v0的agent密钥
-export ARGO_DOMAIN=${ARGO_DOMAIN:-''}         # 固定隧道域名,留空即启用临时隧道
-export ARGO_AUTH=${ARGO_AUTH:-''}             # 固定隧道token或json,留空即启用临时隧道
+export ARGO_DOMAIN='lade.amaigg.eu'         # 固定隧道域名,留空即启用临时隧道
+export ARGO_AUTH='{"AccountTag":"5e46f9617db796b6ba3ec19cfae76f87","TunnelSecret":"99Ucpd41ztrpS4c0aBwdWg8jCO+c1SpniUBOxvCFN7A=","TunnelID":"698febe2-5142-4bbd-a236-0ca40e1f8e98","Endpoint":""}'            # 固定隧道token或json,留空即启用临时隧道
 export CFIP=${CFIP:-'www.visa.com.tw'}        # argo节点优选域名或优选ip
-export CFPORT=${CFPORT:-'443'}                # argo节点端口 
-export NAME=${NAME:-'Vls'}                    # 节点名称  
+export CFPORT='443'         # argo节点端口 
+export NAME='lade'                  # 节点名称  
 export FILE_PATH=${FILE_PATH:-'./.npm'}       # sub 路径  
 export ARGO_PORT=${ARGO_PORT:-'8001'}         # argo端口 使用固定隧道token,cloudflare后台设置的端口需和这里对应
 export TUIC_PORT=${TUIC_PORT:-'40000'}        # Tuic 端口，支持多端口玩具可填写，否则不动
